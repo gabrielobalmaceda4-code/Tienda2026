a2e6dd98ab365ad3bd785fb2c60ed2f22389b670
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-20 14:13:46 +0100

    Tienda a 20/01/2026 CREACIÓN DE CLASE Pedido

---
+++ src/main/java/es/educastur/givanbr90/tienda2026/Pedido.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.givanbr90.tienda2026;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class Pedido {
+
+    private String idPedido;
+    private Cliente clientePedido;
+    private final LocalDate fechaPedido;
+    private ArrayList<LineaPedido> cestaCompra;
+
+    public Pedido(String idPedido, Cliente clientePedido, LocalDate fechaPedido,
+            ArrayList<LineaPedido> cestaCompra) {
+        this.idPedido = idPedido;
+        this.clientePedido = clientePedido;
+        this.fechaPedido = fechaPedido;
+        this.cestaCompra = cestaCompra;
+    }
+
+}
