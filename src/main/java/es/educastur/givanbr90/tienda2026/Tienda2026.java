dd4d40b20354d17cb6f0a8cbae932399b8f99766
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-20 14:12:45 +0100

    Tienda a 20/01/2026 CREACIÓN DE CLASES

---
+++ src/main/java/es/educastur/givanbr90/tienda2026/Tienda2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.educastur.givanbr90.tienda2026;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class Tienda2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
