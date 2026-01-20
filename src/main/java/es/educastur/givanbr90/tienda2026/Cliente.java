1397f69f1af1d40faa4828e2341c6f23bf26ff49
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-20 14:13:06 +0100

    Tienda a 20/01/2026 CREACIÓN DE CLASE Cliente

---
+++ src/main/java/es/educastur/givanbr90/tienda2026/Cliente.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.givanbr90.tienda2026;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class Cliente {
+    private String idCliente;
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Cliente(String idCliente, String nombre, String telefono, String email) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public Cliente(String idCliente, String nombre, String email) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "idCliente=" + idCliente + ", nombre=" + nombre + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+    
+}
