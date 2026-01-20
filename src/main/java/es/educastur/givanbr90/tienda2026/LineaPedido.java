f76be77fd52815b65bbacb7c431131df23815c50
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-20 14:13:35 +0100

    Tienda a 20/01/2026 CREACIÓN DE CLASE LineaPedido

---
+++ src/main/java/es/educastur/givanbr90/tienda2026/LineaPedido.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.educastur.givanbr90.tienda2026;
+
+/**
+ *
+ * @author 1dawd17
+ */
+public class LineaPedido {
+    private String idArticulo;
+    private int unidades;
+
+    public LineaPedido(String idArticulo, int unidades) {
+        this.idArticulo = idArticulo;
+        this.unidades = unidades;
+    }
+
+    @Override
+    public String toString() {
+        return "LineaPedido{" + "idArticulo=" + idArticulo + ", unidades=" + unidades + '}';
+    }
+    
+}
