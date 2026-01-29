3d38c2e1462a107ce269c895f0977c446c8d108e
1dawd17 <1dawd17@A204PC17.desarrolloweb.local>
2026-01-29 14:20:20 +0100

    Tienda a 29/01/2026 SE AÑADEN EXCEPCIONES: -StockCero -StockInsuficiente -nuevoPedido de forma más compleja añadiendo las posibles excepciones

--- src/main/java/es/educastur/givanbr90/tienda2026/LineaPedido.java
+++ src/main/java/es/educastur/givanbr90/tienda2026/LineaPedido.java
@@ -17,6 +17,23 @@ public class LineaPedido {
         this.unidades = unidades;
     }
 
+    public String getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(String idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+    
+
     @Override
     public String toString() {
         return idArticulo + " - " + unidades + " uds.";
